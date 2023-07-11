c81fd9e178205fc70361949c14c47f9b7abaf249
Cheekibreaki <70823386+Cheekibreaki@users.noreply.github.com>
2023-07-11 18:35:23 -0400

    init commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='DWG_to_GeoJson',
+    version='1.0',
+    packages=['gdal'],
+    url='',
+    license='',
+    author='Peter',
+    author_email='',
+    description=''
+)
