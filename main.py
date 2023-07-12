b075cf92d5183b77998f160e5749abb86e484039
Cheekibreaki <70823386+Cheekibreaki@users.noreply.github.com>
2023-07-12 00:02:41 -0400

    Implement basic geometry loading from dxf

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+from osgeo import ogr
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+dxf_file = r"E:\react-naitve project\campus\src\assets\Bahen Indoor building layout\DXF\Drawing1test.dxf"
+driver = ogr.GetDriverByName("DXF")
+dataSource = driver.Open(dxf_file, 0)
+layer = dataSource.GetLayer()
+for feature in layer:
+    geometry = feature.GetGeometryRef()
+    print(geometry)
+# layer = dataSource.GetLayer()
