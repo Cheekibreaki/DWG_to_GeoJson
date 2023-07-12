3c15d76fe91a41c36c4d1f3117a4a428dfa83731
Cheekibreaki <70823386+Cheekibreaki@users.noreply.github.com>
2023-07-12 17:43:33 -0400

    Implement basic geometry loading from dxf

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from osgeo import ogr
 
 dxf_file = r"E:\react-naitve project\campus\src\assets\Bahen Indoor building layout\DXF\Drawing1test.dxf"
-driver = ogr.GetDriverByName("DXF")
-dataSource = driver.Open(dxf_file, 0)
-layer = dataSource.GetLayer()
-for feature in layer:
+dxf_driver = ogr.GetDriverByName("DXF")
+dxf_dataSource = dxf_driver.Open(dxf_file, 1)
+dxf_layer = dxf_dataSource.GetLayer()
+for dxf_layer in dxf_dataSource:
+    dxf_layer_name = dxf_layer.GetName()
+    print(dxf_layer_name)
+for dxf_feature in dxf_layer:
+    dxf_geometry = dxf_feature.GetGeometryRef()
+    print(dxf_geometry)
+# geojson_file = r"E:\react-naitve project\campus\src\assets\Bahen Indoor building layout\DXF\Drawing1test.geojson"
+geojson_driver = ogr.GetDriverByName("GeoJSON")
+Geojson_dataSource = geojson_driver.CreateDataSource("output.geojson")
+Geojson_layer = dxf_dataSource.CopyLayer(dxf_layer, "output")
+for feature in Geojson_layer:
     geometry = feature.GetGeometryRef()
     print(geometry)
 # layer = dataSource.GetLayer()
---
+++ main2.py
@@ -0,0 +1,31 @@
+from osgeo import ogr
+
+dxf_file = r"E:\react-naitve project\campus\src\assets\Bahen Indoor building layout\DXF\Drawing1test.dxf"
+output_dir = r"E:\react-naitve project\campus\src\assets\Bahen Indoor building layout\DXF\\"
+dxf_driver = ogr.GetDriverByName("DXF")
+dxf_dataSource = dxf_driver.Open(dxf_file, 1)
+for dxf_layer in dxf_dataSource:
+    layer_name = dxf_layer.GetName()
+    print(layer_name)
+    output_geojson = output_dir + layer_name + ".geojson"
+    print(output_geojson)
+    # Create a new GeoJSON file for the current layer
+    geojson_driver = ogr.GetDriverByName("GeoJSON")
+    geojson_dataSource = geojson_driver.CreateDataSource(output_geojson)
+
+    # Create a new layer in the GeoJSON file
+    geojson_layer = geojson_dataSource.CreateLayer(layer_name, geom_type=ogr.wkbUnknown)
+
+    # Loop through the features in the current layer and convert them to GeoJSON
+    for feature in dxf_layer:
+        geometry = feature.GetGeometryRef()
+        geojson_feature = ogr.Feature(geojson_layer.GetLayerDefn())
+        geojson_feature.SetGeometry(geometry)
+        geojson_layer.CreateFeature(geojson_feature)
+        geojson_feature = None
+
+    # Clean up and close the data sources for the current layer
+    geojson_dataSource = None
+
+# Clean up and close the data source for the DXF file
+dataSource = None
