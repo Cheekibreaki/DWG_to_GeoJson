b6a97a89365bd91dcd9a3e91b0f333bd3ae46627
Cheekibreaki <70823386+Cheekibreaki@users.noreply.github.com>
2023-07-15 18:13:07 -0400

    Implement Catch label
    Co-authored-by: Su1-yvahn <Su1-yvahn@users.noreply.github.com>

--- main2.py
+++ main2.py
@@ -1,18 +1,37 @@
 from osgeo import ogr
 
 
-def findFeatureForLabel (label_geometry,dxf_layer):
-    for feature in dxf_layer:
-        geometry = feature.GetGeometryRef()
-        type = geometry.GetGeometryType()
-        
-        if(type == ogr.wkbPolygon or type == ogr.wkbLineString or type == ogr.wkbMultiLineString):
-            xmin, xmax, ymin, ymax = geometry.GetEnvelope() 
-            if(label_geometry.GetX()<xmax and 
-               label_geometry.GetX()>xmin and 
-               label_geometry.GetY()>ymin and 
-               label_geometry.GetY()<ymax    ):
-                return feature.GetFID()
+def findFeatureForLabel (label_geometry):
+    
+    dxf_file = r"E:\react-naitve project\campus\src\assets\Bahen Indoor building layout\DXF\Drawing1test.dxf"
+    dxf_driver = ogr.GetDriverByName("DXF")
+    dxf_dataSource = dxf_driver.Open(dxf_file, 1)
+    
+    # # Create a new DXF dataset
+    # new_dxf_driver = ogr.GetDriverByName("DXF")
+    # new_dxf_dataSource = new_dxf_driver.CreateDataSource(new_dxf_file)
+
+    # # Copy the features from the original layer to the new dataset
+    # new_layer_name = "Copy of " + layer_name
+    # new_layer = new_dxf_dataSource.CopyLayer(dxf_layer, new_layer_name)
+
+    # # Clean up and close the new dataset
+    for dxf_layer in dxf_dataSource:
+        for feature in dxf_layer:
+            geometry = feature.GetGeometryRef()
+            type = geometry.GetGeometryType()
+            type_str = ogr.GeometryTypeToName(type)
+            # print(type_str)
+            if(type_str == "Line String"):
+                # print("find feature for label")
+                xmin, xmax, ymin, ymax = geometry.GetEnvelope() 
+                
+                if(xmax > label_geometry.GetX() > xmin and
+                    ymin < label_geometry.GetY() < ymax):
+                    dxf_dataSource = None
+
+                    return  feature.GetFID()
+    dxf_dataSource = None
     return None
            
         
@@ -34,6 +53,10 @@ for dxf_layer in dxf_dataSource:
     # Create a new layer in the GeoJSON file
     geojson_layer = geojson_dataSource.CreateLayer(layer_name, geom_type=ogr.wkbUnknown)
 
+    field_defn = ogr.FieldDefn( "room", ogr.OFTString )
+    field_defn.SetWidth( 32 )
+    geojson_layer.CreateField ( field_defn )
+
     # Loop through the features in the current layer and convert them to GeoJSON
     for feature in dxf_layer:
         feature_id = feature.GetFID()
@@ -44,22 +67,27 @@ for dxf_layer in dxf_dataSource:
         
         geometry = feature.GetGeometryRef()
         type = geometry.GetGeometryType()
-        # type_str = ogr.GeometryTypeToName(type)
-        
-        if(type == ogr.wkbPoint):
-            featureIDIncludeLabel = findFeatureForLabel(geometry,dxf_layer)
+        type_str = ogr.GeometryTypeToName(type)
+        print("typestr:", type_str)
+        if(type_str == "3D Point"):
+            
+            featureIDIncludeLabel = findFeatureForLabel(geometry)
             if(featureIDIncludeLabel != None):
                 print("Feature ID include label is ", featureIDIncludeLabel)
+        else:
+        #field_index = layer.GetLayerDefn().GetFieldIndex(field_name)    
+            geojson_feature = ogr.Feature(geojson_layer.GetLayerDefn())
+            geojson_feature.SetField("room", "foo")
+            geojson_feature.SetGeometry(geometry)
             
-            
-        geojson_feature = ogr.Feature(geojson_layer.GetLayerDefn())
-        geojson_feature.SetGeometry(geometry)
-        geojson_layer.CreateFeature(geojson_feature)
+            geojson_layer.CreateFeature(geojson_feature)
         
         geojson_feature = None
 
+    # featureIDIncludeLabel = findFeatureForLabel(label_geometry,dxf_layer)
+
     # Clean up and close the data sources for the current layer
-    geojson_dataSource = None
+geojson_dataSource = None
     # print(geojson_layer.GetFeature(0))
 # Clean up and close the data source for the DXF file
-dataSource = None
+dxf_dataSource = None
