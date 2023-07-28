476a1183b322743c899c3275bf4cfe5b8bcc78e0
Cheekibreaki <70823386+Cheekibreaki@users.noreply.github.com>
2023-07-28 14:29:58 -0400

    Verison 2 Completion
    Fixing bugs and add new features

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from osgeo import ogr
 import collections
 from osgeo import ogr, osr
+import os
 import math
+import json
 
-
-dxf_folder_list = [{
+dxf_folder_list = \
+[{
     "layer_name" : "BA_Indoor_1",
+    "floor" : 1 ,
+    "contour_color" : [148, 148, 148] ,
+    "room_color" : [255, 225, 143] ,
     "path" : r"E:\react-naitve project\campus\src\assets\Bahen Indoor building layout\Bahen - 1stFloor",
     "reference_lat" : 43.65923740619973,
     "reference_lon" : -79.39784311817914,
@@ -13,7 +18,21 @@ dxf_folder_list = [{
     "x_offset" : -7,
     "y_offset" : -2.5,
     "height" : 30
+},{
+    "layer_name" : "BA_Indoor_2",
+    "floor" : 2 ,
+    "contour_color" : [148, 148, 148] ,
+    "room_color" : [255, 225, 143] ,
+    "path" : r"E:\react-naitve project\campus\src\assets\Bahen Indoor building layout\Bahen - 2ndFloor",
+    "reference_lat" : 43.65923740619973,
+    "reference_lon" : -79.39784311817914,
+    "bearing_diff" : 16.5,
+    "x_offset" : -7,
+    "y_offset" : -2.5,
+    "height" : 30
 }]
+
+all_building_json = "all_building"
 
 output_dir = r"E:\react-naitve project\campus\src\assets\geojson\\"
 
@@ -85,7 +104,8 @@ def findFeatureForLabel (dxf_file,label_geometry,roomNUM,store_info):
     dxf_dataSource = None
     return None
            
-def writeGeoJson (dxf_file,layer_name,geo_params,isContour):
+def writeGeoJson (dxf_file,layer_name,floor_num,color,geo_params,is_all_building):
+
     reference_lat, reference_lon,  bearing_diff, height, x_offset,y_offset = geo_params       
     print("dxf_file")
     print("dxf_file",dxf_file)
@@ -94,24 +114,43 @@ def writeGeoJson (dxf_file,layer_name,geo_params,isContour):
     print(dxf_dataSource)
     for dxf_layer in dxf_dataSource:
         print(layer_name)
-        output_geojson = output_dir + layer_name + ".json"
+        if(is_all_building):
+            output_geojson = output_dir + all_building_json + ".json"
+        else:
+            output_geojson = output_dir + layer_name + ".json"
         print(output_geojson)
         # Create a new GeoJSON file for the current layer
         geojson_driver = ogr.GetDriverByName("GeoJSON")
-        geojson_dataSource = geojson_driver.CreateDataSource(output_geojson)
+        json_is_exist = False
+        if os.path.exists(output_geojson):
+            json_is_exist = True
+        if json_is_exist:
+            geojson_dataSource = geojson_driver.Open(output_geojson, 1)
+        else:
+            geojson_dataSource = geojson_driver.CreateDataSource(output_geojson)
 
         # Create a new layer in the GeoJSON file
         geojson_layer = geojson_dataSource.CreateLayer(layer_name, geom_type=ogr.wkbUnknown)
 
-        field_defn = ogr.FieldDefn( "room", ogr.OFTString )
-        field_defn.SetWidth( 32 )
-        geojson_layer.CreateField ( field_defn )
-        field_defn = ogr.FieldDefn( "height", ogr.OFTInteger )
-        field_defn.SetWidth( 32 )
-        geojson_layer.CreateField ( field_defn )
-        field_defn = ogr.FieldDefn( "base_height", ogr.OFTInteger )
-        field_defn.SetWidth( 32 )
-        geojson_layer.CreateField ( field_defn )
+        if not json_is_exist:
+            field_defn = ogr.FieldDefn( "room", ogr.OFTString )
+            field_defn.SetWidth( 32 )
+            geojson_layer.CreateField(field_defn)
+            field_defn = ogr.FieldDefn("floor", ogr.OFTString)
+            field_defn.SetWidth(32)
+            geojson_layer.CreateField(field_defn)
+            field_defn = ogr.FieldDefn("color", ogr.OFTString)
+            field_defn.SetWidth(32)
+            geojson_layer.CreateField(field_defn)
+            field_defn = ogr.FieldDefn("layer_name", ogr.OFTString)
+            field_defn.SetWidth(32)
+            geojson_layer.CreateField ( field_defn )
+            field_defn = ogr.FieldDefn( "height", ogr.OFTInteger )
+            field_defn.SetWidth( 32 )
+            geojson_layer.CreateField ( field_defn )
+            field_defn = ogr.FieldDefn( "base_height", ogr.OFTInteger )
+            field_defn.SetWidth( 32 )
+            geojson_layer.CreateField ( field_defn )
 
         feature_with_label = {}
 
@@ -155,13 +194,32 @@ def writeGeoJson (dxf_file,layer_name,geo_params,isContour):
                     result=calculate_new_coordinates(reference_lat, reference_lon,coords[i][1], coords[i][0],  bearing_diff, height, x_offset,y_offset )
                     print(result)
                     geometry.SetPoint_2D(i,result[0],result[1])
-                geojson_feature.SetGeometry(geometry)          
-                geojson_feature.SetField("height", height)  
-                geojson_feature.SetField("base_height", height)  
+
+                coords = []
+                for i in range(geometry.GetPointCount()):
+                    coords.append(geometry.GetPoint_2D(i))
+
+                polygon = ogr.Geometry(ogr.wkbPolygon)
+                linear_ring = ogr.Geometry(ogr.wkbLinearRing)
+
+                for coord in coords:
+                    linear_ring.AddPoint(*coord)
+
+                linear_ring.CloseRings()
+                polygon.AddGeometry(linear_ring)
+
+                geojson_feature.SetGeometry(polygon)
+                geojson_feature.SetField("layer_name", layer_name)
+                geojson_feature.SetField("height", height)
+                geojson_feature.SetField("base_height", height)
+                geojson_feature.SetField("color", str(color))
+                geojson_feature.SetField("floor", floor_num)
                 geojson_layer.CreateFeature(geojson_feature)
 
-            geojson_feature = None
 
+
+            geojson_feature = None
+        geojson_dataSource = None
 
         # Clean up and close the data sources for the current layer
     geojson_dataSource = None
@@ -170,14 +228,51 @@ def writeGeoJson (dxf_file,layer_name,geo_params,isContour):
     dxf_dataSource = None
 
 
+def combine_json_files(input_files, output_file):
+    combined_data = []
+    for file_path in input_files:
+        try:
+            with open(file_path, 'r') as file:
+                data = json.load(file)  # Load JSON data from file into a Python dictionary
+                print("data",data["features"][0])
+                for feature in data["features"]:
+                    combined_data.append(feature)
+                # combined_data.update(data)  # Merge dictionaries
+        except (IOError, json.JSONDecodeError) as e:
+            print(f"Error processing file '{file_path}': {e}")
+    json_content = {"features":combined_data, "type": "FeatureCollection"}
+
+    with open(output_file, 'w') as outfile:
+        json.dump(json_content, outfile, indent=2)  # Use indent for pretty formatting
+
 
 if __name__ == "__main__":
+    try:
+        for filename in os.listdir(output_dir):
+            file_path = os.path.join(output_dir, filename)
+            if os.path.isfile(file_path):
+                os.remove(file_path)
+                print(f"Deleted file: {file_path}")
+    except OSError as e:
+        print(f"Error: {e}")
+
+
+
     for dxf in dxf_folder_list:
         contour_path = dxf["path"]+"\Contour.dxf"
         room_path = dxf["path"]+"\Room.dxf"
         contour_layer_name = dxf["layer_name"]+"_contour"
         room_layer_name = dxf["layer_name"]+"_room"
-        writeGeoJson(room_path,room_layer_name,[dxf["reference_lat"],dxf["reference_lon"],dxf["bearing_diff"],dxf["height"],dxf["x_offset"],dxf["y_offset"]])
-        writeGeoJson(contour_path,contour_layer_name,[dxf["reference_lat"],dxf["reference_lon"],dxf["bearing_diff"],dxf["height"],dxf["x_offset"],dxf["y_offset"]])
-        
+        room_color = dxf["room_color"]
+        contour_color = dxf["contour_color"]
+        floor_num = dxf["floor"]
+        writeGeoJson(room_path,room_layer_name,floor_num,room_color,[dxf["reference_lat"],dxf["reference_lon"],dxf["bearing_diff"],dxf["height"],dxf["x_offset"],dxf["y_offset"]],False)
+        writeGeoJson(contour_path,contour_layer_name,floor_num,contour_color,[dxf["reference_lat"],dxf["reference_lon"],dxf["bearing_diff"],dxf["height"],dxf["x_offset"],dxf["y_offset"]],False)
+
+    input_files = []
+    for filename in os.listdir(output_dir):
+        file_path = os.path.join(output_dir, filename)
+        input_files.append(file_path)
+    print("input",input_files)
+    combine_json_files(input_files,output_dir + all_building_json + ".json")
         
